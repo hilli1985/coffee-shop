bde8f2dbd70598aa0ac3ed0638cb4dfabbb44cc9
Hilla Meir <hillameir85@gmail.com>
2018-07-29 12:54:31 +0300

    add main.js after changes

--- main.js
+++ main.js
@@ -1,20 +1,53 @@
 var coffeeShop = {
-  beans: 40,
+    beans: 40,
+    money: 100,
+    beanPrice: 1,
+    drinkRequirements: {
+        latte: 10,
+        americano: 5,
+        doubleShot: 15,
+        frenchPress: 12,
+    },
 
-  drinkRequirements: {
-    latte: 10,
-    americano: 5,
-    doubleShot: 15,
-    frenchPress: 12
-  },
+    makeDrink: function(drinkType) {
+        if (coffeeShop.drinkRequirements[drinkType] === undefined) {
+            alert("Sorry, we don't make " + drinkType);
+        } else if (this.beans >= this.drinkRequirements[drinkType]) {
+            this.beans -= this.drinkRequirements[drinkType];
+            console.log(drinkType + " is ready!");
+        } else {
+            alert("Sorry, we're all out of beans");
+        }
+    },
 
-  makeDrink: function (drinkType) {
-    // TODO: Finish this method
-  }
+    buySupplies: function(units) {
+        this.money -= (this.beanPrice * units);
+        this.beans += units
+    },
+    buyDrink: function(drinkType) {
+        if (coffeeShop.drinkRequirements[drinkType] === undefined) {
+            alert("Sorry, we don't make " + drinkType);
+        } else if (this.beans >= this.drinkRequirements[drinkType]) {
+            this.money += (this.beanPrice * this.drinkRequirements[drinkType]);
+            alert("you need to pay: " + this.beanPrice * this.drinkRequirements[drinkType]);
+        } else {
+            alert("Sorry, we're all out of beans");
+        }
+    }
 }
 
+
 coffeeShop.makeDrink("latte");
-coffeeShop.makeDrink("americano");
-coffeeShop.makeDrink("filtered");
-coffeeShop.makeDrink("doubleShot");
+coffeeShop.buyDrink("latte");
+//console.log(coffeeShop.money);
+//coffeeShop.makeDrink("americano");
+//coffeeShop.buyDrink("americano");
+//coffeeShop.makeDrink("filtered");
+//coffeeShop.buyDrink("filtered");
+//coffeeShop.makeDrink("doubleShot");
+//coffeeShop.buyDrink("doubleShot");
+coffeeShop.buySupplies(10);
+// now after we add more beans we can make coffee 
 coffeeShop.makeDrink("frenchPress");
+coffeeShop.buyDrink("frenchPress");
+console.log(coffeeShop.money);
